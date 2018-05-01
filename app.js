aefafbf806cc203b4fc9537625027b457c984cb4
Ryan Khatib <ryan.khatib@gmail.com>
2018-05-01 15:10:20 -0700

    restarted app.js

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
-function guessMyName() {
-    var answer = prompt('Guess my name?');
-    console.log('user guessed', answer);
+'use strict';
 
-    var p = document.getElementById('my-name-response');
-
-    if(answer.trim().toLowerCase() === 'marty') {
-        p.textContent = 'You\'re right! My name is Marty';
-    }
-    else {
-        p.textContent = 'Sorry, not my name. Try again';
-    }
-}
