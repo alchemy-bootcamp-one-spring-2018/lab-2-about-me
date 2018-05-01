dc6d5494f2701e0a6481e923609b64169aa6ed56
Kevin Grayson <k.grayson.ms@gmail.com>
2018-05-01 14:08:58 -0700

    got guessing game function completed

--- app.js
+++ app.js
@@ -2,29 +2,39 @@
 
 //eslint-disable-next-line
 function guessMyName() {
+    //counter value started at 1
     var i = 1;
+    //while loop giving user 3 "guesses" (counter less than 4)
     while(i < 4) {
+        //prompts the user to input a "guess" when the function is called
         var answer = prompt('Guess my name?');
+        //logs console with user's guess and returns current counter value
         console.log('User guessed:', answer);
         console.log('Counter at:', i);
 
         var p = document.getElementById('my-name-response');
+        //after trimming white space and converting to lower case, validates user guess against correct answer
         if(answer.trim().toLowerCase() === 'kevin') {
+            //if correct, displays winning message; breaks
             p.textContent = 'You\'re right! My name is Kevin!';
             break;
         }
+        //else returns message/hint #1; increments counter value
         else {
             if(i === 1){
                 alert('Sorry, not my name. Try again. Starts with a "K"!');
                 i++;
             }
+            //else returns message/hint #2; increments counter value
             else {
                 if(i === 2) {
-                    alert('Not quite! Starts with a "K", ends with an "N", and it is 5 letters long...');
+                    alert('Not quite! Starts with a "K", ends with an "N", and my name is 5 letters long...');
                     i++;
                 }
+                //maximum attempts (3) reached; displays losing message; breaks
                 else {
-                    p.textContent = 'Sorry bro! You died (i.e. you ran out of guesses)!';
+                    p.textContent = 'You died! (you ran out of guesses)';
+                    break;
                 }
             }
         }
