60d813149c7a7515fbcf47f8717ea4630d835e76
KLPainter <KLPainter@gmail.com>
2018-05-01 14:33:06 -0700

    animal game complete

--- games.js
+++ games.js
@@ -1,14 +1,17 @@
 function guessMyAnimal() {
-    // this is where we will write a response to the user; reset it to blank.
+
+    // this is where we will write a response to the user; reset it to blank
     var p = document.getElementById('animal-answer');
-    p.textContent = '';
+    p.textContent = '-'; // this has no effect for some reason
+
     // get the user's guess
     var guess = prompt ('What is my favorite kind of animal?\nHint: They are llarge and llovely.', 'hedgehog?');
-    // let the user know if they are correct.
-    if(guess === null || guess.trim === '')
+
+    // let the user know if they are correct
+    if(guess === null || guess.trim() === '')
     {
         p.textContent = ('Ok, you don\'t have to play.');
-    } else if(guess.toLowerCase === 'llama' || guess.toLowerCase === 'llamas'){
+    } else if(guess.toLowerCase() === 'llama' || guess.toLowerCase() === 'llamas') {
         p.textContent = ('You are correct!  I llove llamas.');
     } else {
         p.textContent = ('No, sorry.  I llove llamas.');
