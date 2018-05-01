da0702ab60bf3ce17d75b5d706ad62a9af4f2d8f
Mark Myers <markalope@gmail.com>
2018-05-01 14:48:53 -0700

    wrote questions (pseudocode) into JS file

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 'use strict';
-console.log('JS file is linked properly');
 
+
+// Game One - Yes or No
+console.log('beginning of Game One');
+
+//Does Mark like Dogs better than Cats.
+
+//Does Mark like Star Wars?
+
+//Has Mark ever worked at McDonald's
+
+//Does Mark drink a crapload of Coffee?
+
+//Would Mark like Sushi for lunch?
+
+
+
+
+// Game Two - Three Guesses
+console.log('beginning of Game Two');
+
+//In Which East Coast state was I born?
