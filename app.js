e080c123ff7c5896b463f87126aea44642b2f58f
Kevin Grayson <k.grayson.ms@gmail.com>
2018-05-01 12:22:48 -0700

    cleaned up app.js

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
+'use strict';
+
 //eslint-disable-next-line
-function isMyName() {
-    var answer = prompt('Is my name Marty?');
+function guessMyName() {
+    var answer = prompt('Guess my name?');
     console.log('user guessed', answer);
+
     var p = document.getElementById('my-name-response');
 
-    if(answer.trim().toLowerCase() === 'marty')
-        p.textContent = "You\'re right! My name is Marty";
+    if(answer.trim().toLowerCase() === 'marty') {
+        p.textContent = 'You\'re right! My name is Marty';
     }
     else {
-        p.textContent = "Sorry, not my name. Try again.";
+        p.textContent = 'Sorry, not my name. Try again.';
     }
+}
 
+//eslint-disable-next-line
 function catLover() {
     var p = document.getElementById('cat-lover-response');
     p.textContent = 'Of course, everyone likes cats!';
-}
-
 }
