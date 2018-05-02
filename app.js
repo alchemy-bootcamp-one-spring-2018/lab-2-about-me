647385d31fb7a6764fa8a3c3f6b7ad632f2c53f8
Ryan Luras <ryanluras@gmail.com>
2018-05-01 23:24:19 -0700

    I think ive got everything runing at least

--- app.js
+++ app.js
@@ -1,83 +1,12 @@
 'use strict';
 
-
 var quiz = [
     'Are you a citizen?',
     'Do you have loyalty to the human race?',
     'You see a turtle lying on its back in the hot sun, do you flip it over?',
-    'There is am illegal replicant living next door to you, do you turn it in to the authorities?',
+    'There is an illegal replicant living next door to you, do you turn it in to the authorities?',
     'Do you approve of the Tyrell Corp. leadership?'
 ];
-
-
-// eslint-disable-next-line
-function reportReplicant() {
-    var answer = prompt('Knowledge of Replicant: Verification Required. What model is the subject?');
-    console.log('user guessed', answer);
-
-    var p = document.getElementById('report-response');
-
-    if(answer.trim().toLowerCase() === 'nexus 6') {
-        p.textContent = 'Model Confirmed';
-        var answer2 = prompt('Which planet colony was the subject assigned to?');
-        console.log('user guessed', answer2);
-        var p = document.getElementById('report-response');
-
-        if(answer2.trim().toLowerCase() === 'titan') {
-            p.textContent = 'Planet Confirmed';
-            var answer3 = prompt('What is the name of the replicant para-military faction?');
-            console.log('user guessed', answer3);
-            var p = document.getElementById('report-response');
-
-            if(answer3.trim().toLowerCase() === 'arm of titan') {
-                p.textContent = 'Faction Confirmed';
-                var answer4 = prompt('What is the name of the replicant martyr honored by this faction');
-                console.log('user guessed', answer4);
-                var p = document.getElementById('report-response');
-
-                if(answer4.trim().toLowerCase() === 'roy batty') {
-                    p.textContent = 'Martyr Confirmed';
-                    var p = document.getElementById('report-response');
-                    p.textContent = ' 4 of 4 correct. User faction knowledge sufficient. Proceed to Update.';
-                }
-                else {
-                    p.textContent = 'Incorrect Input, 3 of 4 correct. User tagged for inspection.';
-                }
-            }
-            else {
-                p.textContent = 'Incorrect Input, 2 of 4 correct. User tagged for inspection.';
-            }
-        }
-        else {
-            p.textContent = 'Incorrect Input, 1 of 4 correct. User tagged for inspection.';
-        }
-    }
-    else {
-        p.textContent = 'Incorrect Input, 0 of 4 correct. User tagged for inspection.';
-    }
-}
-// eslint-disable-next-line
-function factionLocation() {
-    var p = document.getElementById('location-response');
-    p.textContent = 'Current telemetry indicates Arm of Titan faction cannot be at this location. User tagged for inspection.';
-}
-// eslint-disable-next-line
-function factionLocation2() {
-    var p = document.getElementById('location-response');
-    p.textContent = 'Current telemetry indicates Arm of Titan faction cannot be at this location. User tagged for inspection.';
-}
-// eslint-disable-next-line
-function factionLocation3() {
-    var p = document.getElementById('location-response');
-    p.textContent = 'Current telemetry indicates Arm of Titan faction cannot be at this location. User tagged for inspection.';
-}
-// eslint-disable-next-line
-function factionLocation4() {
-    var p = document.getElementById('location-response');
-    p.textContent = 'POSSIBLE LOCATION RECORDED';
-    alert('THANK YOU CITIZEN');
-}
-
 
 function connect() {
     for(var i = 0; i < 5; i++) {
@@ -89,3 +18,75 @@ function connect() {
     }
 }
 connect();
+// eslint-disable-next-line
+function reportReplicant() {
+    var answer = prompt('Knowledge of Replicant: Verification Required. What model is the subject?');
+    console.log('user guessed', answer);
+    var p = document.getElementById('report-response');
+
+    if(answer.trim().toLowerCase() === 'nexus 6') {
+        answer = 1;
+        var answer2 = prompt('Which planet colony was the subject assigned to?');
+        console.log('user guessed', answer2);
+        var p = document.getElementById('report-response');
+
+        if(answer2.trim().toLowerCase() === 'titan') {
+            answer = 2;
+            var answer3 = prompt('What is the name of the replicant para-military faction?');
+            console.log('user guessed', answer3);
+            var p = document.getElementById('report-response');
+
+            if(answer3.trim().toLowerCase() === 'arm of titan') {
+                answer = 3;
+                var answer4 = prompt('What is the name of the replicant martyr honored by this faction');
+                console.log('user guessed', answer4);
+                var p = document.getElementById('report-response');
+
+                if(answer4.trim().toLowerCase() === 'roy batty') {
+                    answer = 4;
+                    var answer5 = prompt('What sector was the replicant last seen near?');
+                    console.log('user guessed', answer5);
+                    var p = document.getElementById('report-response');
+                    
+                    if(answer5.trim().toLowerCase() === 'ceres') {
+                        answer = 5;
+                        p.textContent = ('Total Evaluation: ' + answer + '/5' + ' (' + Math.floor((answer / 5) * 100) + '%)');
+                    }
+                }
+                else {
+                    p.textContent = ('INCORRECT INPUT: ' + answer + '/5' + ' (' + Math.floor((answer / 5) * 100) + '%)');
+                }
+            }
+            else {
+                p.textContent = ('INCORRECT INPUT: ' + answer + '/5' + ' (' + Math.floor((answer / 5) * 100) + '%)');
+            }
+        }
+        else {
+            p.textContent = ('INCORRECT INPUT: ' + answer + '/5' + ' (' + Math.floor((answer / 5) * 100) + '%)');
+        }        
+    }
+    else {
+        p.textContent = ('INCORRECT INPUT: ' + answer + '/5' + ' (' + Math.floor((answer / 5) * 100) + '%)');   
+    }
+}
+// eslint-disable-next-line
+function factionLocation() {
+    var p = document.getElementById('location-response');
+    p.textContent = 'Current telemetry indicates Arm of Titan faction is not on Titan. User tagged for inspection.';
+}
+// eslint-disable-next-line
+function factionLocation2() {
+    var p = document.getElementById('location-response');
+    p.textContent = 'Current telemetry indicates Arm of Titan faction is not on Sirius. User tagged for inspection.';
+}
+// eslint-disable-next-line
+function factionLocation3() {
+    var p = document.getElementById('location-response');
+    p.textContent = 'Current telemetry indicates Arm of Titan faction is not on Europa. User tagged for inspection.';
+}
+// eslint-disable-next-line
+function factionLocation4() {
+    var p = document.getElementById('location-response');
+    p.textContent = 'POSSIBLE LOCATION RECORDED';
+    alert('THANK YOU CITIZEN');
+}
