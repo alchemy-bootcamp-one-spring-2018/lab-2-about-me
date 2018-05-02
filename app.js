92b01b79874d3b20bb74287c5e38a4a79b162b64
Zachar13y <zachar13y@gmail.com>
2018-05-02 00:25:39 -0700

    Added guessMyCity function, fixed correct response to guess animal question.

--- app.js
+++ app.js
@@ -6,35 +6,36 @@ function guessMyName(){
 
     var p = document.getElementById('my-name-response');
 
-    if(answer.trim().toLowerCase() === 'zack') {
+    if(answer.trim().toLowerCase() === 'zack'){
         alert('Correct');
         p.textContent = 'You\'re right! My name is Zack';
     }
     else {
         alert ('Sorry, not my name.');
-        p.textContent = 'Wrong!'
+        p.textContent = 'Wrong';
     }
 }
 
 function guessMyAnimal(){
-
-    for(var i=2; i<0, i--;){
-        var answer = prompt('What is my favorite animal (you have 2 guesses)');
+    // for(var i=2; i<0, i--;){
+        var answer = prompt('What is my favorite animal? (you have 2 guesses)');
         console.log('User guessed', answer);
-    if(answer.trim().toLowerCase()=== 'hawk') {
-        alert('Correct');
-        p.textContent = 'Flying high!';
-        }
-    else if(answer.trim().toLowerCase()!== 'hawk')
-            answer = prompt('Try again! (you have 1 guess left!)')
+        
+        var p = document.getElementById('my-animal-response');
 
-    var p = document.getElementById('my-animal-response');
-    
-    // else {
-    //     alert ('Try again! (you have 1 guess left!)');
-        p.textContent = 'Wrong'
-    }
-}
+        if(answer.trim().toLowerCase() === 'hawk'){
+            alert('Correct');
+            p.textContent = 'Flying high!';
+        }
+        else if(answer.trim().toLowerCase() != 'hawk'){
+            answer = prompt('Try again! (you have ' +i+ ' guess left!)');
+            p.textContent = 'Wrong';
+        }
+    }     
+// }
+
+// else {
+//     alert ('Try again! (you have 1 guess left!)');
 
 function guessMyAge(){
     var answer = prompt('How old am I? (3 guesses left!)')
@@ -47,6 +48,21 @@ function guessMyAge(){
         p.textContent = 'You got it!';
     }
     if(answer < 30)
-        alert('C\'mon! I\'m not that old!')
+        alert('C\'mon! I\'m not that old!');
         p.textContent = 'Try again!'
+}
+
+function guessMyCity(){
+    var answer = prompt('What city was I born in?')
+    console.log('User guessed', answer);
+
+    var p = document.getElementById('my-city-response')
+
+    if(answer.trim().toLowerCase() === 'seattle'){
+        alert('Right!');
+        p.textContent = 'Rock on!';
+    }
+    else{
+        alert('Nope!')
+    }
 }
