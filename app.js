310c95a88609cb49ee6a77116968ada1edeef06a
Rehmsy <sarahrehmer@gmail.com>
2018-05-02 08:32:18 -0700

    still not functional. can't get any more done before due

--- app.js
+++ app.js
@@ -1,42 +1,5 @@
 'use strict';
 
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
-
-
-function generateQuiz(questions, quizContainer) {
-    
-    function showQuestions(questions, quizContainer) {
-    
-        }
-
-    var output = [];
-    var answers;
- 
-
-    for(var i=0; i<questions.length; i++) {
-        answers = [];
-
-        for(letter in questions[i].answers){
-
-            answers.push(
-            '<label>'
-            + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-            + letter + ': '
-            + questions[i].answers[letter]
-            + '</label>'
-        );
-    }
-    output.push(
-        '<div class="question">' + questions[i].question + '</div>'
-        + '<div class="answers">' + answers.join('') + '</div>'
-    );
-}
-    quizContainer.innerHTML = output.join('');
-}
-
-
 var myQuestions = [
     {
         question: "Would I rather ",
@@ -65,16 +28,78 @@ var myQuestions = [
     }
 ];
 
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
 
-function showQuestions(questions, quizContainer) {
-    var output = []
-    var answers;
 
-    for(var i=0; i<questions.length; i++) {
-        answers = [];
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
+    
+    function showQuestions(questions, quizContainer) {
+
+        var output = [];
+        var answers;
+
+        for(var i=0; i<questions.length; i++) {
+        
+            answers = [];
+
+            for(letter in questions[i].answers){
+
+                answers.push(
+                    '<label>'
+                        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                + letter + ': '
+                + questions[i].answers[letter]
+                + '</label>'
+                );
+            }
+
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+        quizContainer.innerHTML = output.join('');
     }
+
+
+    function showResults(questions, quizContainer, resultsContainer){
+        
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+        var userAnswer = '';
+        var numCorrect = 0;
+        
+        for(var i = 0; i < questions.length; i++){
+
+            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+        
+            // if correct
+            if(userAnswer === questions[i].correctAnswer){
+            // add points
+                numCorrect++;
+            
+                answerContainers[i].style.color = 'blue';
+            }
+            // if wrong or empty
+            else {
+                answerContainers[i].style.color = 'red';
+            }
+        }
+
+        // number points out of total
+        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+    }
+
+    // for now show questions right away FIX LATER
+    showQuestions(questions, quizContainer);
+
+    submitButton.onclick = function(){
+        showResults(questions, quizContainer, resultsContainer);
+    };
 }
 
-   
+
+
 
 
