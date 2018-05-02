19118b8e974c0b8b96576f0dd0b984864376777a
Mark Myers <markalope@gmail.com>
2018-05-02 14:18:25 -0700

    Inserted dynamic username into all on-screen messaging

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 'use strict';
 
+/* exported getUserName */
 
 // Game One - Yes or No answers
 // eslint-disable-next-line
-var userName = 'Placeholder';
+var userName = 'Friend';
 var score = 0;
 var totalScore = '';
 var message1 = '';
 
+function getUserName(){
+    userName = prompt('Before we start playing, please tell me your name!');
+}
+
 // eslint-disable-next-line
 function gameOne(){
 
-//Does Mark like Dogs better than Cats.  
+    //First, call the function to ask for username
+    getUserName();
+
+    //Does Mark like Dogs better than Cats.  
     var q1 = confirm('Does Mark like Dogs better than Cats?');
     if(q1) {
         var a1 = 'correct';
@@ -33,7 +41,7 @@ function gameOne(){
         a2 = 'wrong';
     }
 
-    console.log('Mark likes Star Wars. I was there in \'77! You got this one ' + a2 + '.');
+    console.log('Mark likes Star Wars. He was there in \'77! You got this one ' + a2 + '.');
 
     //Has Mark ever worked at McDonald's
     var q3 = confirm('Has Mark ever worked at McDonald\'s?');
@@ -71,10 +79,10 @@ function gameOne(){
     totalScore = (score / 5) * 100;
 
     //logs score to the console
-    console.log('You got ' + score + ' out of 5 answers correct for a score of ' + totalScore + '%');
+    console.log(userName + ' got ' + score + ' out of 5 answers correct for a score of ' + totalScore + '%');
 
     //creates message to display on HTML page
-    message1 = 'You got ' + score + ' out of 5 answers correct for a score of ' + totalScore + '%';
+    message1 = userName + 'got ' + score + ' out of 5 answers correct for a score of ' + totalScore + '%';
 
     //sends composed final message and score to the HTML page
     var p = document.getElementById('game-one-result');
@@ -91,35 +99,39 @@ var message2; //this stores the HTML that is inserted into the empty div on inde
 
 //eslint-disable-next-line
 function gameTwo(){
+
+    //First, call the function to ask for username
+    getUserName();
+
     userGuess = prompt('In which East Coast state was Mark born?');
     
     for(actualGuesses = 1; actualGuesses < 5; actualGuesses ++){
         if(userGuess.toLowerCase() === 'new jersey' || userGuess.toLowerCase() === 'nj'){
             
-            alert('Great job! You guessed Right! And it only took ' + actualGuesses + ' tries!');
+            alert('Great job ' + userName + '! You guessed Right! And it only took ' + actualGuesses + ' tries!');
 
-            message2 = ('<p>You guessed right! Mark was born in New Jersey! You guessed in only ' + actualGuesses + ' tries!</p>');
+            message2 = ('<p>Great job ' + userName + '! You guessed right! Mark was born in New Jersey! You guessed in only ' + actualGuesses + ' tries!</p>');
             
             answer = true;
             break;
         } else {
             remainingGuesses = totalGuesses - actualGuesses;
-            userGuess = prompt('Sorry, that\'s not correct. Please Guess again. You have ' + remainingGuesses + ' left.');
+            userGuess = prompt('Sorry, ' + userName + ' that\'s not correct. Please guess again. You have ' + remainingGuesses + ' left.');
         }
         //insert message if number of guesses is exhausted. However, must check for value of answer so this doesn't show everytime.
     }
     //console log results from Game Two
     if(answer === true) {
-        console.log('Mark was born in New Jersey. User Correctly guessed this in ' + actualGuesses + ' tries.'); 
+        console.log('Mark was born in New Jersey. ' + userName + ' correctly guessed this in ' + actualGuesses + ' tries!'); 
     } else {
-        console.log('Mark was born in New Jersey. User took 5 guesses, but did not guess this correctly.');
+        console.log('Mark was born in New Jersey. ' + userName + ' took 5 guesses, but did not guess this correctly.');
     }
 
     //HTML message from Game Two
     if(answer === true) {
-        message2 = 'Mark was born in New Jersey. User Correctly guessed this in ' + actualGuesses + ' tries.'; 
+        message2 = 'Mark was born in New Jersey. ' + userName + ' correctly guessed this in ' + actualGuesses + ' tries.'; 
     } else {
-        message2 = 'Mark was born in New Jersey. User took 5 guesses, but did not guess this correctly.';
+        message2 = 'Mark was born in New Jersey. ' + userName + ' took 5 guesses, but did not guess this correctly.';
     }
     var p = document.getElementById('game-two-result');
     p.textContent = message2;
