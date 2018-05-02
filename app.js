77485d2cc2f32ad9de3f054ec9cca4f3aba55fda
daisywebb31 <webbdaisy31@yahoo.com>
2018-05-02 15:39:16 -0700

    still more changes

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var guessesLeft = 3
 
 // eslint-disable-next-line
 function guessMyName(){
-    for (var i = 0; i < 10; i++) {
-        
+    for (var i = 0; i < 3; i++) {
+
     }
 
     var answer = prompt ('Guess my name?');
@@ -12,7 +12,7 @@ function guessMyName(){
 
     var p = document.getElementById ('my-name-response');
 
-    if (answer.trim().toLowerCase()=== 'liz') {
+    if (answer.trim().toLowerCase()=== 'liz'|| answer.trim().toLowerCase()==='ann'|| answer.trim().toLowerCase()==='webb'|| answer.trim().toLowerCase()==='daisy'){
          alert('You\'re right! My name is Liz');
          
     }
@@ -22,49 +22,17 @@ function guessMyName(){
 
 
     }
-
-    var answer = prompt ('Guess my name?' );
-    console.log ('user guessed', answer);
-
-    if (answer.trim().toLowerCase()=== 'ann') {
-         alert('You\'re right! My name is ann')
-   
-    }
-    else {
-       p.textContent = 'Sorry,not my name.Try again';
-       guessesLeft--
-
-    }
-    var answer = prompt ('Guess my name?');
-    console.log ('user guessed', answer);
-    
-    if (answer.trim().toLowerCase()=== 'webb'){
-        alert('You\'re right! My name is webb')
-
-    }
-    else {
-        p.textContent = 'Sorry,not my name.Try again';
-        guessesLeft--
-    }
-
-    var answer = prompt ('Guess my name?');
-    console.log ('user guessed', answer);
-
-    if (answer.trim().toLocaleLowerCase()=== 'daisy'){
-        alert('You\'re right! My name is daisy')
-
-    }
-    else {
-        p.textContent = 'Sorry,not my name.Try again';
-        guessesLeft--
-
-
-    }
-
-
 }
 
-// function glitterLover () {
-//         var p = document.getElementById ('glitter-lover-response');
-//         p.textContent = 'Of course everyone loves glitter!';
-//     }
+function glitterLover () {
+    var question = prompt ('Are you a glitter lover?');
+    console.log ('user guessed',answer);
+
+    var p = document.getElementById ('glitter-lover-response');
+
+    if (answer.trim().toLowerCase()=== 'yes');{
+        alert ('Sparkle on! I love glitter too');
+    }
+
+
+
