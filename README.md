f1eae437b7128bc1bb96267308ac5a5e77aa48ce
Zachar13y <zachar13y@gmail.com>
2018-05-02 13:37:02 -0700

    Added Readme file, made all of the correct answers work

---
+++ README.md
@@ -0,0 +1,5 @@
+About Me 
+
+1. Title is usually the Repo name. This is a questionnair about my likes and 
+2. Developer facing instructions
+3. 
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//exported guessMyName 
+
 'use strict';
 
 function guessMyName(){
@@ -17,7 +19,7 @@ function guessMyName(){
 }
 
 function guessMyAnimal(){
-    // for(var i=2; i<0, i--;){
+    for(var i=2; i<0, i--;){
         var answer = prompt('What is my favorite animal? (you have 2 guesses)');
         console.log('User guessed', answer);
         
@@ -26,37 +28,43 @@ function guessMyAnimal(){
         if(answer.trim().toLowerCase() === 'hawk'){
             alert('Correct');
             p.textContent = 'Flying high!';
+            break;
         }
         else if(answer.trim().toLowerCase() != 'hawk'){
             answer = prompt('Try again! (you have ' +i+ ' guess left!)');
             p.textContent = 'Wrong';
         }
     }     
-// }
-
-// else {
-//     alert ('Try again! (you have 1 guess left!)');
+}
 
 function guessMyAge(){
-    var answer = prompt('How old am I? (3 guesses left!)')
-    console.log('User guessed', answer);
+    for(var i=4; i<0, i--;){
+        var answer = prompt('How old am I? (3 guesses left!)')
+        console.log('User guessed', answer);
 
-    var p = document.getElementById('my-age-answer');
+        var p = document.getElementById('my-age-answer');
 
-    if(answer == 29){
-        alert('Yess!');
-        p.textContent = 'You got it!';
-    }
-    if(answer < 30)
-        alert('C\'mon! I\'m not that old!');
-        p.textContent = 'Try again!'
+        if(answer == 29){
+            alert('Yess!');
+            p.textContent = 'You got it!';
+            break;
+        }
+        if(answer > 30){
+            alert('C\'mon! I\'m not that old!');
+            p.textContent = 'Try again!'
+        }
+        if(answer < 29){
+            alert('Nope, go higher.')
+        }
+    }    
 }
+
 
 function guessMyCity(){
     var answer = prompt('What city was I born in?')
     console.log('User guessed', answer);
 
-    var p = document.getElementById('my-city-response')
+    var p = document.getElementById('my-city-response');
 
     if(answer.trim().toLowerCase() === 'seattle'){
         alert('Right!');
@@ -66,3 +74,38 @@ function guessMyCity(){
         alert('Nope!')
     }
 }
+
+function guessMyShow() {
+    var answer = prompt('What do you think my favorite show is?');
+    console.log('User guessed', answer);
+
+    var p = document.getElementById('my-show-response');
+
+
+    if(answer.trim().toLowerCase() === 'rick and morty'){
+        alert('Oh yeah! Get schwifty!');
+        p.textContent = 'Wubbalubba dub dub!';
+    }
+    else{
+        alert('Never heard of it. Try again!')
+    }
+}
+
+
+// for(var i = 0; i < 3; i++){
+//     var answer = prompt('What is my favorite loop?');
+//     console.log(answer);
+    
+    
+//     if(answer === 'while'){
+//         alert('Yes, while is my favorite loop.')
+//         break;
+//     }
+
+//     if(i === 3){
+//         alert('Sorry my favorite loop is a while loop.');
+//     }
+//     else {
+//         alert('Sorry, my favorite loop is a while loop.');
+//     }
+// }
