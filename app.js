55e3da2d668ed1e65749a625d49b833e370c9f7f
rbtprograms <rbt61690@gmail.com>
2018-05-02 14:44:47 -0700

    first second branch commit

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-function guessYesNo(){
-    var i = 0;
+'use strict';
 
+/* exported guessYesNo lastName favoriteAnimal rockPaperScissors*/
+//This is the code for a 5 question yes or no guessing game
+function guessYesNo(){
+    //variable i increases with every correct answer, score is displayed at end
+    var i = 0;
+    //all questions follow some basic premise. Prompt, console log, if else statement for correct and incorrect answers, responses with alert
     var mexicoAnswer = prompt('Do you think I\'ve been to Mexico?').trim().toLowerCase();
     console.log('user guessed', mexicoAnswer);
     if(mexicoAnswer === 'yes' || mexicoAnswer === 'y'){
@@ -22,10 +27,10 @@ function guessYesNo(){
     var driveAnswer = prompt('Do you think I like to drive?').trim().toLowerCase();
     console.log('user guessed', driveAnswer);
     if(driveAnswer === 'no' || driveAnswer === 'n'){
-        alert("Correct! Driving is the worst!");
+        alert('Correct! Driving is the worst!');
         i++;
     } else {
-        alert("Incorrect! Driving is the worst!")
+        alert('Incorrect! Driving is the worst!');
     }
 
     var petAnswer = prompt('Do I have any pets?').trim().toLowerCase();
@@ -45,27 +50,83 @@ function guessYesNo(){
     } else {
         alert('How do you think I made this?!');
     }
-
+    //This displayed the final score on the screen
     p.textContent = 'You got ' + i + ' questions right! Good job!';
 }
-
+//variable is later changed to display something different based upon answers
 var p = document.getElementById('yes-no-response');
-
-
+//simply game of guessing my last name. user has 5 chances, responses change at 1 and 0 chances left.
 function lastName(){
-    for(var i=5; i >= 0; i--){
-        var lastNameQ = prompt('What is my last name?');
+    var p = document.getElementById('last-name-response');
+    for(var i = 4; i >= 0; i--){
+        var lastNameQ = prompt('What is my last name?').trim().toLowerCase();
         console.log('user guessed', lastNameQ);
-        if(lastNameQ === 'Thompson'){
+        if(lastNameQ === 'thompson'){
             alert('You are a stalker');
-            var p = document.getElementById('last-name-response');
             p.textContent = 'Seriously. Huge stalker.';
-            {break;}
-        } else {
-            alert('Nope! Keep trying! You have ' + i + ' chances left!');
-            var p = document.getElementById('last-name-response');
-            p.textContent = 'Better luck next time!'
+            break;
+        } else if(i > 1){
+            alert('Nope! Keep trying! You have ' + i + ' chances left! HINT: First letter T.');
+        } else if(i === 1){
+            alert('Nope! Keep trying! You have ' + i + ' chance left! HINT: Thomp..');
+        } else if(i === 0){
+            alert('Sorry, you are out of chances!');
+            p.textContent = 'Better luck next time!';
+            break;
         }
     }
-    
+}
+/* Admittedly, I did not get particularly creative with this one. This is largely similar to what Marty showed us in class. I figured typing it out and playing with it
+a little would give me a better idea of how exactly it worked, and I have a fair grasp of how this game works now. But it;s not much different from what Marty showed us,
+just a couple of extra steps and potential responses.*/
+var animalGuesses = 3;
+function favoriteAnimal(){
+    var animalInput = document.getElementById('animal-input');
+    var response = document.getElementById('animal-response');
+    var animal = animalInput.value.trim().toLowerCase();
+
+    animalGuesses--;
+
+    if(animal === 'eagle'){
+        response.textContent = 'Wow you\'re right! Great guess!';
+        document.getElementById('animal-btn').disabled = true;
+    } else if(animalGuesses === 2){
+        response.textContent = 'Nope! Try something that flies!';
+    } else if(animalGuesses === 1){
+        response.textContent = 'Nah, try something BIG that flies!';
+    } else if(animalGuesses === 0){
+        response.textContent = 'No! Try refreshing the page to try again!';
+        document.getElementById('animal-btn').disabled = true;
+    }
+}
+
+function rockPaperScissors(entry){
+    var choices = ['rock', 'paper', 'scissors'];
+    var compSelection = function(){
+        var compChoice = choices[Math.floor(Math.random() * 3)];
+        return compChoice;
+    };
+    var response = document.getElementById('rps-display');
+
+    var choice = compSelection();
+    console.log(choice);
+    if(entry === 'rock' && choice === 'scissors'){
+        response.textContent = 'Scissors! You won!';
+    } else if(entry === 'rock' && choice === 'paper'){
+        response.textContent = 'Paper! You lose!';
+    } else if(entry === 'rock' && choice === 'rock'){
+        response.textContent = 'Rock! Draw!';
+    } else if(entry === 'paper' && choice === 'scissors'){
+        response.textContent = 'Scissors! You lose!';
+    } else if(entry === 'paper' && choice === 'paper'){
+        response.textContent = 'Paper! Draw!';
+    } else if(entry === 'paper' && choice === 'rock'){
+        response.textContent = 'Rock! You win!';
+    } else if(entry === 'scissors' && choice === 'scissors'){
+        response.textContent = 'Scissors! Draw!';
+    } else if(entry === 'scissors' && choice === 'paper'){
+        response.textContent = 'Paper! You win!';
+    } else if(entry === 'scissors' && choice === 'rock'){
+        response.textContent = 'Rock! You lose!';
+    }
 }
