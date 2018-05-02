a850fc1709c0c9fcb9e12117b9056d5c75d76790
Zachar13y <zachar13y@gmail.com>
2018-05-01 18:03:55 -0700

    made a for loop for one question, modified prompts and alerts.

--- app.js
+++ app.js
@@ -17,18 +17,36 @@ function guessMyName(){
 }
 
 function guessMyAnimal(){
-    newFunction();
-    var answer = prompt('What is my favorite animal (you have 2 guesses)');
-    console.log('User guessed', answer);
 
-    var p = document.getElementById('my-animal-response');
-
+    for(var i=2; i<0, i--;){
+        var answer = prompt('What is my favorite animal (you have 2 guesses)');
+        console.log('User guessed', answer);
     if(answer.trim().toLowerCase()=== 'hawk') {
         alert('Correct');
         p.textContent = 'Flying high!';
-    }
-    else {
-        alert ('Try again!');
+        }
+    else if(answer.trim().toLowerCase()!== 'hawk')
+            answer = prompt('Try again! (you have 1 guess left!)')
+
+    var p = document.getElementById('my-animal-response');
+    
+    // else {
+    //     alert ('Try again! (you have 1 guess left!)');
         p.textContent = 'Wrong'
     }
+}
+
+function guessMyAge(){
+    var answer = prompt('How old am I? (3 guesses left!)')
+    console.log('User guessed', answer);
+
+    var p = document.getElementById('my-age-answer');
+
+    if(answer == 29){
+        alert('Yess!');
+        p.textContent = 'You got it!';
+    }
+    if(answer < 30)
+        alert('C\'mon! I\'m not that old!')
+        p.textContent = 'Try again!'
 }
