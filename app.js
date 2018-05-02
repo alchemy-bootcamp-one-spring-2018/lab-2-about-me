2a658cbbee86f9880f15a215427cb4cc8de10edb
Rehmsy <sarahrehmer@gmail.com>
2018-05-02 06:51:32 -0700

    started adding css and added more js.

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 'use strict';
 
+var questionNum = 0;
+var quizScore =0;
+
 // eslint-disable-next-line
-function wouldIrather() {
-    var answer = promt('Would I Rather');
+function wouldIratherQ1() {
+    var answer = prompt('Would I Rather');
+    console.log('user guessed', answer);
+}
+
+function wouldIratherQ2() {
+    var answer = prompt('Would I Rather');
+    console.log('user guessed', answer);
+}
+
+function wouldIratherQ3() {
+    var answer = prompt('Would I Rather');
+    console.log('user guessed', answer);
+}
+
+function wouldIratherQ4() {
+    var answer = prompt('Would I Rather');
+    console.log('user guessed', answer);
+}
+
+function wouldIratherQ5() {
+    var answer = prompt('Would I Rather');
     console.log('user guessed', answer);
 }
