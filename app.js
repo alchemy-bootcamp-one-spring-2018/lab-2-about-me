254f27c964d6b124c7147d646a69eece65126009
daisywebb31 <webbdaisy31@yahoo.com>
2018-05-02 17:40:02 -0700

    more changes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict';
-var guessesLeft = 3
+var guessesLeft = 3;
 
 // eslint-disable-next-line
 function guessMyName(){
-    for (var i = 0; i < 3; i++) {
+    for(var i = 0; i < 3; i++) {
 
     }
 
@@ -12,27 +12,53 @@ function guessMyName(){
 
     var p = document.getElementById ('my-name-response');
 
-    if (answer.trim().toLowerCase()=== 'liz'|| answer.trim().toLowerCase()==='ann'|| answer.trim().toLowerCase()==='webb'|| answer.trim().toLowerCase()==='daisy'){
-         alert('You\'re right! My name is Liz');
-         
+    if(answer.trim().toLowerCase() === 'liz' || answer.trim().toLowerCase() === 'ann' || answer.trim().toLowerCase() === 'webb' || answer.trim().toLowerCase() === 'daisy'){
+        alert('You\'re right! My name is Liz');
+
     }
     else {
         p.textContent = 'Sorry,not my name. Try again';
-        guessesLeft--
+        guessesLeft--;
 
 
     }
 }
 
-function glitterLover () {
-    var question = prompt ('Are you a glitter lover?');
-    console.log ('user guessed',answer);
+function allaboutliz() {
+    var score = 0;
+    var question1 = prompt ('do I have blue hair').trim().toLowerCase();
+    console.log ('user guessed', question1);
 
-    var p = document.getElementById ('glitter-lover-response');
+    if(question1 === 'yes'){
+        score++;
+        console.log ('your score is', score);
+        alert('Hells yeah!');
 
-    if (answer.trim().toLowerCase()=== 'yes');{
-        alert ('Sparkle on! I love glitter too');
     }
+    else {
+        score--;
+        alert('duh yeah I have blue hair');
+    }
+    console.log(score);
+
+    var question2 = prompt ('am I married').trim().toLowerCase();
+    console.log ('user guessed', question2);
+    
+    if(question2 === 'yes') {
+        score++;
+        alert('to my bestfriend!');
+
+    }
+    else {
+        score--;
+        alert('totally married!');
+    }
+    console.log(score);
+
+    var question3 = prompt ('Am I allergic to peanuts').trim().toLowerCase();
+}
+
+
 
 
 
